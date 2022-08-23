0ade6a94970be0a1b03902cffbbd6655aaf88462
JINU98 <jinendramalekar@gmail.com>
2022-08-23 17:03:05 -0400

    opensource

---
+++ docs/opensource/Showcase _ Kno.e.sis_files/panels.js
@@ -0,0 +1,64 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+
+(function ($) {
+  Drupal.Panels = {};
+
+  Drupal.Panels.autoAttach = function() {
+    if ($.browser.msie) {
+      // If IE, attach a hover event so we can see our admin links.
+      $("div.panel-pane").hover(
+        function() {
+          $('div.panel-hide', this).addClass("panel-hide-hover"); return true;
+        },
+        function() {
+          $('div.panel-hide', this).removeClass("panel-hide-hover"); return true;
+        }
+      );
+      $("div.admin-links").hover(
+        function() {
+          $(this).addClass("admin-links-hover"); return true;
+        },
+        function(){
+          $(this).removeClass("admin-links-hover"); return true;
+        }
+      );
+    }
+  };
+
+  $(Drupal.Panels.autoAttach);
+})(jQuery);
+
+
+}
+/*
+     FILE ARCHIVED ON 09:51:08 Mar 16, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:26:18 Aug 23, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 1560.589
+  exclusion.robots: 0.085
+  exclusion.robots.policy: 0.076
+  cdx.remote: 0.063
+  esindex: 0.009
+  LoadShardBlock: 1454.375 (3)
+  PetaboxLoader3.datanode: 1592.268 (5)
+  CDXLines.iter: 14.24 (3)
+  load_resource: 500.476 (2)
+  PetaboxLoader3.resolve: 354.397 (2)
+*/
