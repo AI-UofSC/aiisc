5a72750edffeb0a21220df00db8b69b6bf406ab2
JINU98 <jinendramalekar@gmail.com>
2023-06-30 11:33:07 -0400

    achivements

--- docs/amit/achievement/main_0.php
+++ docs/amit/achievement/main_0.php
@@ -1,6 +1,38 @@
 <h1>Recognition, Outstanding Achievements, and Honorary Appointments</h1>
 <ol style="text-align:justify">
 
+<li>
+        <a href="https://www.computer.org/volunteering/awards/mcdowell">IEEE Computer Society W. Wallace McDowell Award</a> &ldquo;for pioneering and enduring contributions to information integration, data and service semantics, and knowledge-enhanced computing,&rdquo; 2023.&nbsp;<a href="https://sc.edu/uofsc/posts/2023/03/center_integrates_ai_into_research.php">Univ News</a>
+    </li>
+    <li>
+        ACM Fellow, elected by the Association for Computing Machinery&nbsp;<a href="https://awards.acm.org/award-recipients/sheth_2299642">&ldquo;for contributions to data semantics and knowledge-enhanced computing&rdquo;</a>, 2020.&nbsp;<a href="https://photos.app.goo.gl/53TKEhznDADNfYhg6">ACM Profile</a> <a href="https://sc.edu/study/colleges_schools/engineering_and_computing/news_events/news/2021/sheth-named-acm-fellow.php">Univ News</a>
+    </li>
+    <li>
+        IEEE TCSVC Research Innovation Award, &ldquo;in &ldquo;recognition of his pioneering and enduring research, applications and adoption of distributed workflow processes and semantics in services computing,&rdquo; 2020.&nbsp;<a href="http://tab.computer.org/tcsvc/activities/">IEEE Page</a> <a href="https://www.cse.sc.edu/news/dr-sheth-wins-ieee-tcsvc-research-innovation-award-2020">Univ Announcement</a>
+    </li>
+    <li>
+        Member,&nbsp;<a href="https://ndews.org/about/sag/">Scientific Advisory Group, NDEWS</a> (NIDA&rsquo;s National Drug Early Warning System) Coordinating Center, June 2020 - .
+    </li>
+    <li>
+        Distinguished Alumni Award for Academic Excellence, Franklin College of Art &amp; Science, The Ohio State University, 2019.&nbsp;<a href="https://awards.webapps.engineering.osu.edu/awards/18">OSU Announcement</a> <a href="https://photos.app.goo.gl/efGY2BDMhUYQ7WcN8">Description</a> <a href="https://photos.app.goo.gl/vQZfim8bd3Edy2Pw5">Award Photo</a>
+    </li>
+    <li>
+        AAAS Fellow&nbsp;elected by the American Association for the Advancement of Sciences &ldquo;for pioneering and enduring contributions on information integration, distributed workflow, and semantics and knowledge-based big data analytics.&rdquo;&nbsp;<a href="https://www.aaas.org/news/aaas-honors-accomplished-scientists-2018-elected-fellows">Class of 2018</a>.&nbsp;<a href="https://webapp2.wright.edu/web1/newsroom/2018/11/27/wright-state-professor-amit-sheth-elected-a-fellow-of-american-association-for-the-advancement-of-sciences/">University News</a>
+    </li>
+    <li>
+        AAAI Fellow&nbsp;elected by the Association for the Advancement of Artificial Intelligence &ldquo;for significant and enduring contributions to &nbsp;semantics and knowledge based techniques to transform diverse data into insights and actions.&rdquo;&nbsp;<a href="https://aaai.org/about-aaai/aaai-awards/the-aaai-fellows-program/elected-aaai-fellows/">Class of 2018</a>.&nbsp;<a href="https://webapp2.wright.edu/web1/newsroom/2018/11/27/wright-state-professor-amit-sheth-elected-a-fellow-of-american-association-for-the-advancement-of-sciences/">Univ Media/News</a> <a href="https://photos.app.goo.gl/R95hY712bg5dbr236">Award</a>
+    </li>
+    <li>
+        Top 50/80 Computer Scientist in the USA/World, Research.com, 2022.
+    </li>
+    <li>
+        2021 IEEE Internet Computing, Best Paper Award for &ldquo;Semantics of the Black-Box: Can Knowledge Graphs Help Make Deep Learning Systems More Interpretable and Explainable?&rdquo; by Manas Gaur, Keyur Faldu and Amit Sheth. <a href="https://photos.app.goo.gl/TCa1szu7NoCBhW5T6">Photo</a>
+    </li>
+    <li>
+        Semantic Web Science Association/Intl Semantic Web Conf 10 year award for paper: Jain et al &rdquo;Ontology Alignment for Linked Open Data&rdquo;, 2010 proceedings/2020 award. <a href="https://swsa.semanticweb.org/content/swsa-ten-year-award">Announcement</a>
+    </li>
+
+
 <li>Technology Commercialization Award (runner up), Ohio Faculty Council, 13 Nov 2017. The honor recognizes faculty members in the state university system in Ohio for exceptional research discoveries and the role they have played in supporting the translation of those discoveries into marketable products and/or services.  [<a href="https://www.ohiofacultycouncil.org/sites/ohiofacultycouncil.wright.edu/files/page/attachments/Award-Release-Sheth.pdf">Citation</a>,<a href="http://knoesis.org/amit/media/files/certificate.jpg"> Certificate</a>,<a href="http://knoesis.org/amit/media/files/OFC-Award-Profile-Nov2017.pdf"> Article</a>]
 </li>
 
